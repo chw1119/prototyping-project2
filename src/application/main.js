884b5c9ff55fa0fb67daa456c0849b8d24d3cb84
chw1119 <chw1119@hanyang.ac.kr>
2025-01-11 22:36:52 +0900

    create nodejs app

--- src/application/main.js
+++ src/application/main.js
@@ -2,8 +2,15 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 
-app.get('/', function(req,res){
+// 'public' 폴더를 static 파일 디렉토리로 설정
+app.use(express.static('./front'));
 
-})
+// '/client' 경로로 요청이 들어오면 HTML 파일을 응답으로 반환
+app.get('/client', function(req, res) {
+    res.render('index.html');
+});
 
-app.listen(8080)
+// 8080 포트에서 서버 실행
+app.listen(8080, () => {
+    console.log("서버가 8080 포트에서 실행 중입니다.");
+});
